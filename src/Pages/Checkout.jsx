8a71c803d2d7d1cdb3b78aaf2283b9500ba727ed
Andrew O'Reilly <andreworeilly04@gmail.com>
2026-04-22 11:31:16 -0400

    g

--- src/Pages/Checkout.jsx
+++ src/Pages/Checkout.jsx
@@ -83,7 +83,7 @@ const Checkout = ({ cart, setCart }) => {
 
         const orderData = {
             items: cart,
-            total: cart.reduce((acc, item) => acc + item.price * item.quantity, 0),
+            total: total,
             createdAt: serverTimestamp(),
             status: 'Processing',
             user: currentUser.uid,
