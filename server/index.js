927e3ec23067af60f517603699859286e53fd717
Andrew O'Reilly <andreworeilly04@gmail.com>
2026-04-23 00:56:04 -0400

    git

--- server/index.js
+++ server/index.js
@@ -145,9 +145,10 @@ app.use(express.json());
 // 💰 HELPER: CALCULATE TOTAL
 const calculateTotal = (items) => {
     
-    const subtotal = items.reduce((acc, item) => {return acc + (Number(item.price) * Number(item.quantity));}, 0);
+    const subtotal = items.reduce((acc, item) => { return acc + (Number(item.price) * Number(item.quantity)); }, 0);
      const tax = subtotal * 0.10;
      const shipping = 5.99;
+     
      return subtotal + tax + shipping;
 };
 
