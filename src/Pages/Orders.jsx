6b2385f66de091d6807cc8ca799615f90202dc21
Andrew O'Reilly <andreworeilly04@gmail.com>
2026-04-23 12:56:17 -0400

    git

--- src/Pages/Orders.jsx
+++ src/Pages/Orders.jsx
@@ -58,7 +58,7 @@ const Orders = () => {
                 <h3 className="orders__title">Your Orders</h3>
 
                 {loading ? (
-                    <p>Loading orders...</p>
+                    <p className="loading">Loading orders...</p>
                 ) : orders.length === 0 ? (
                     <div className="empty_orders--container">
                         <h3 className="empty__orders">You don't have any orders yet</h3>
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -45,7 +45,7 @@ const Product = ({ AllProducts, addToCart, cart, isLoggedIn }) => {
     };
 
 
-    if (!product) return <div>Product not found!</div>;
+    if (!product) return <div className="not_found">Product not found!</div>;
 
 
 
