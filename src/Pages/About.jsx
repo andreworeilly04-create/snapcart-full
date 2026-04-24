733a39996d6db4bc64d57e87117c30479da328ad
Andrew O'Reilly <andreworeilly04@gmail.com>
2026-04-24 12:57:05 -0400

    g

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -14,12 +14,12 @@ const About = () => {
             <img className="about__img" src={aboutImg} alt="About" />
           </figure>
           <div className="about__text">
-          <p className="about__paragraph">Snapcart is a user-friendly e-commerce website designed for fast and convenient online shopping. It features a clean interface with easy navigation through sections like Home, Products, About, and Contact. Users can browse items, add them to a cart, and complete purchases smoothly. Snapcart focuses on simplicity, accessibilty, and a seamless shopping experience across devices.</p>
+          <p className="about__paragraph">Snapcart is a user-friendly E-commerce website designed for fast and convenient online shopping. It features a clean interface with easy navigation through sections like Home, Products, About, and Contact. Users can browse items, add them to a cart, and complete purchases smoothly. Snapcart focuses on simplicity, accessibilty, and a seamless shopping experience across devices.</p>
           <h3 className="why_choose_us">Why Choose Snap<span>Cart</span></h3>
           <div className="features">
             <div className="feature-box">
               <h3>24/7 Customer Support</h3>
-              <p>Our Support team is available around the clock to help you with aby questions or issues.</p>
+              <p>Our Support team is available around the clock to help you with any questions or issues.</p>
             </div>
              <div className="feature-box">
               <h3>Fast Delivery</h3>
