d02c37d56d6812c7eb8a93fff4cd74036cc3cbe3
Andrew O'Reilly <andreworeilly04@gmail.com>
2026-05-03 23:58:57 -0400

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,7 @@ function App() {
   const [searchTerm, setSearchTerm] = useState("");
   const [showPassword, setShowPassword] = useState(false);
 
-  const [isProcessing, setIsProcessing] = useState(false);
+  const [isProcessing, setIsProcessing] = useState(true);
 
   const toggleMenu = () => setIsMenuOpen((prev) => !prev);
   const toggleSearch = () => setIsInputOpen((prev) => !prev);
