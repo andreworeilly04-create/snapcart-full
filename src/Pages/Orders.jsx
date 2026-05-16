83eb2ac74e7921c5ea7d7d6e6af61499c395c010
Andrew O'Reilly <andreworeilly04@gmail.com>
2026-05-16 14:52:35 -0400

    Update Orders.jsx

--- src/Pages/Orders.jsx
+++ src/Pages/Orders.jsx
@@ -139,7 +139,7 @@ const Orders = () => {
                                 <p className="order_total">
                                     Total: ${Number(order.amount || 0).toFixed(2)}
                                 </p>
-                                {!["Shipping Now", "Shipped", "Out for Delivery", "Delivered"].includes(order.status) && (
+                                {!["Preparing For Shipment" "Shipping Now", "Shipped", "Out for Delivery", "Delivered"].includes(order.status) && (
                                 <button onClick={() => handleCancelClick(order.id)} className="cancel">Cancel Order</button>)}
 
                                 {["Shipping Now", "Shipped", "Out for Delivery", "Delivered"].includes(order.status) && (<p className="status-notice">This order is {order.status} and cannot be canceled.</p>)}
