1f4f2a38908388e1a138d9d534f0f101c09b9603
Andrew O'Reilly <andreworeilly04@gmail.com>
2026-07-15 15:13:34 -0400

    Add line_items to Stripe checkout session creation

--- server/index.js
+++ server/index.js
@@ -201,6 +201,7 @@ app.post('/create-checkout-session', async (req, res) => {
         const session = await stripe.checkout.sessions.create({
             payment_method_types: ['card'],
             mode: 'payment',
+            line_items,
             success_url: `${CLIENT_URL}/orders`,
             cancel_url: `${CLIENT_URL}/checkout`,
             metadata: {
